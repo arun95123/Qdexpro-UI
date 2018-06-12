d912638f55d1bd738d1b4c55b919eadcfb86a29d
Prema Balamurugan <a.prema.balamurugan@accenture.com>
2018-06-12 17:27:49 +0530

    Redesigned with accordion style

--- client/components/MiddlePane/middlePane.js
+++ client/components/MiddlePane/middlePane.js
@@ -4,29 +4,42 @@ import './middlePane.Style.scss'
 const middlePane =() =>(
     <div className='middle-pane'>
         <div className='middle-pane--content'>
-            Scenario
-            <button className='middle-pane--content--button'>Edit</button>
-        </div>
-        <hr />
-        <div className='middle-pane--content'>
-            Add Scenario Component
+            <p className='middle-pane--content--caret--right'>
+                <i className="caret right icon"></i></p>
+            <p className='middle-pane--content--text'>Scenario</p>
+            <p className='middle-pane--content--edit'>
+                <i className="edit icon"></i></p>
             <button className='middle-pane--content--button'>Add</button>
         </div>
         <hr />
         <div className='middle-pane--content'>
-            Setup
-            <p className='middle-pane--content--triangle--down'/>
+            <p className='middle-pane--content--caret--right'>
+                <i className="caret right icon"></i></p>
+            <p className='middle-pane--content--text'>Setup</p>
+            <button className='middle-pane--content--button'>Add</button>
         </div>
         <div className='middle-pane--subcontent'>
+            <i className="angle right icon"></i>
             <p>Step 1</p>
         </div>
         <hr />
         <div className='middle-pane--content'>
-            Test Case
-            <p className='middle-pane--content--triangle--down'/>
+            <p className='middle-pane--content--caret--right'>
+                <i className="caret right icon"></i></p>
+            <p className='middle-pane--content--text'>Test Case</p>
+            <p className='middle-pane--content--edit'>
+                <i className="edit icon"></i></p>
+            <button className='middle-pane--content--button'>Add</button>
+        </div>
+        <div className='middle-pane--content'>
+            <p className='middle-pane--content--caret--right'>
+                <i className="caret right icon"></i></p>
+            <p className='middle-pane--content--text'>Setup</p>
+            <button className='middle-pane--content--button'>Add</button>
         </div>
         <div className='middle-pane--subcontent'>
-            Add Test Case Component
+            <i className="angle right icon"></i>
+            <p>Step 1</p>
         </div>
         <hr />
 
