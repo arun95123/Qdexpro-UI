378b22633a300e42d221041b65dce3b324ee0acd
PB <aravindhan.p.b@gmail.com>
2018-06-16 16:12:40 +0530

    Added Edit option

--- client/components/MiddlePane/Scenario/scenario.js
+++ client/components/MiddlePane/Scenario/scenario.js
@@ -13,25 +13,44 @@ const click3 =() => (
     console.log('Tear Down is clicked')
 )
 
-const scenario =() =>(
-  <div className='middle-pane--content'>
-      <p className='middle-pane--content--caret--right'>
-          <i className="caret right icon"></i></p>
-      <p className='middle-pane--content--text'>Scenario</p>
-      <p className='middle-pane--content--edit'>
-          <i className="edit icon"></i></p>
-      <button className='middle-pane--content--button'>
-      <div className="ui dropdown">
-         Add
-        <i className="dropdown icon"></i>
-        <div className="menu">
-            <div className="item" data-value="0" onClick={click1}>Setup</div>
-            <div className="item" data-value="1" onClick={click2}>Test Case</div>
-            <div className="item" data-value="2" onClick={click3}>Tear Down</div>
+
+class scenario extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {editing: false};
+    this.edit = this.edit.bind(this);
+    this.changeIcon = this.changeIcon.bind(this);
+  }
+
+  edit(){
+    this.setState({editing: !this.state.editing});
+  }
+
+  changeIcon(){
+    return (this.state.editing) ? "save icon" : "edit icon" ;
+  }
+
+  render() {
+    return(
+      <div className='middle-pane--content'>
+          <p className='middle-pane--content--text' contenteditable="true">Scenario</p>
+          <p className='middle-pane--content--edit'>
+              <i className={this.changeIcon()} onClick={this.edit}></i></p>
+          <button className='middle-pane--content--button'>
+          <div className="ui dropdown">
+             Add
+            <i className="dropdown icon"></i>
+            <div className="menu">
+                <div className="item" data-value="0" onClick={click1}>Setup</div>
+                <div className="item" data-value="1" onClick={click2}>Test Case</div>
+                <div className="item" data-value="2" onClick={click3}>Tear Down</div>
+            </div>
         </div>
-    </div>
-      </button>
-    </div>
-  );
+          </button>
+        </div>
+      );
+    }
+  }
+
 
 export default scenario;
