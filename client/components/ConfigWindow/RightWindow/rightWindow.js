fd2db2346a883e4cd017d4ead3d0b8cce02bdc21
Venky <ezio123arun@gmail.com>
2018-09-01 23:24:02 +0530

    Adding Editor

--- client/components/ConfigWindow/RightWindow/rightWindow.js
+++ client/components/ConfigWindow/RightWindow/rightWindow.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './rightWindow.Style.scss'
 import { Form, TextArea } from 'semantic-ui-react'
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/java';
+import 'brace/theme/github';
 
 class rightWindow extends React.Component{
   constructor(props){
@@ -8,24 +13,37 @@ class rightWindow extends React.Component{
     this.showTextArea = this.showTextArea.bind(this);
   }
 
-  showTextArea(expanded){
-    if(!expanded)
-    {
-      return <TextArea placeholder='Edit Code Here!' rows='670px' cols='78' />
-    }
-    else {
-      return <TextArea placeholder='Edit Code Here!' rows='670px' cols='125' />
-    }
+  onChange(newValue) {
+    console.log('change',newValue);
+  }
+
+  windowWidth(expanded){
+    return expanded ? "900px" : "570px";
   }
 
 
-render(){
-  const {textAreaExpand} = this.props;
-  return(
-    <div className='rightWindow'>
-    {this.showTextArea(textAreaExpand)}
-    </div>
-  )
-}
+  showTextArea(expanded){
+      return (
+        <AceEditor
+          mode="java"
+          theme="github"
+          onChange={this.onChange}
+          name="codeEditor"
+          editorProps={{$blockScrolling: true}}
+          height="auto"
+          width={this.windowWidth(expanded)}
+        />
+      );
+    }
+
+
+  render(){
+    const {textAreaExpand} = this.props;
+    return(
+      <div className='rightWindow'>
+      {this.showTextArea(textAreaExpand)}
+      </div>
+    )
+  }
 }
 export default rightWindow
