3e90a15263cbad74249875cc4de2f9f1dbb03619
Venky <ezio123arun@gmail.com>
2018-11-08 23:17:38 +0530

    Refactor node layer, minor changes in webpack, using express handlebars, start:dev task

--- app/index.js
+++ app/index.js
@@ -1,52 +1,26 @@
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('../webpack.config.js');
-const router = require('../api/router');
+const exphbs =require('express-handlebars');
+const router = require('./router');
 const bodyParser=require('body-parser');
 const app = express();
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 
-if (isDeveloping) {
-  const compiler = webpack(config);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    contentBase: 'src',
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  });
+app.use(express.static(path.join(__dirname, '..', '/dist')));
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-  app.get('/', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-    res.end();
-  });
-    app.use(express.static('public'));
-} else {
-  app.use(express.static(__dirname + '/dist'));
-  app.get('/', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.engine('.html', exphbs({
+  defaultLayout: 'layout',
+  extname: '.html'
+}));
+app.set('view engine', '.html');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 app.use('/',router);
+
+app.get('/*', function response(req, res) {
+  res.render('home');
+});
 
 module.exports = app;
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,18 @@
 
 var path = require('path');
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval-source-map',
   entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'client')
+   'babel-polyfill', path.join(__dirname, 'client')
   ],
   output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: '[name].js',
+    path: path.resolve(process.cwd(), 'dist'),
+    filename: 'bundle.js',
     publicPath: '/'
   },
 
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'client/views/index.html',
-      inject: 'body',
-      filename: 'index.html'
-    }),
-    new webpack.HotModuleReplacementPlugin()
   ],
 
   module: {
@@ -32,24 +23,27 @@ module.exports = {
       exclude: /node_modules/,
       use: [{
          loader: 'babel-loader',
-         options: { presets: ["react", "es2015", "stage-0", "react-hmre"] }
+         options: { presets: ["react", "es2015", "stage-0"] }
        }]
-    },
+     },
      {
-      test: /\.json?$/,
-      use: [{
-        loader : 'json-loader'
-      }]
-    },
-    { test: /\.(css)$/, use: [ 'style-loader','css-loader'] },
-    {
-            test: /\.scss$/,
-            use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS
-            ]
-    }
-  ]
- }
+       test: /\.json?$/,
+       use: [{
+         loader : 'json-loader'
+       }]
+      },
+      {
+        test: /\.(css)$/,
+        use: [ 'style-loader','css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS
+        ]
+      }
+    ]
+  }
 };
