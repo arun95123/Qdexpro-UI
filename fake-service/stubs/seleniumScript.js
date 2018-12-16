3ebaeb17d0773d9e1ad9af9ef06377c39f230267
Navaneethan sukumaran <39336151+navaneethan123@users.noreply.github.com>
2018-12-16 17:41:38 +0530

    changing code format in stub

--- fake-service/stubs/seleniumScript.js
+++ fake-service/stubs/seleniumScript.js
@@ -1,6 +1,6 @@
 module.exports.getSeleniumScript = () => {
     return(
       {button:
-        'This is a Java Code\\nPublic Void Main'
+        `package SeleniumFramework.TestSuite;\nimport org.openqa.selenium.By;\nimport org.openqa.selenium.WebElement;\nimport org.testng.annotations.Test;\nimport CoreControlClasses.ButtonMain;\nimport WebDriverCoreSupportClasses.WebDriverClass;\npublic class Test002 {\n@Test\npublic static void f() {\nWebElement element = WebDriverClass.findClickableElement(By.xpath("//span[text()='Tools']"));\nbutton.ButtonClick(element);\n}\n}`
       })
 };
