73be7a468ff83df49d5614f5ec329e3454812769
Manikandan <manikandan@Manikandans-MacBook-Air.local>
2019-02-17 11:44:00 +0530

    Homescreen changes

--- client/components/HelloWorld/HelloWorld.js
+++ client/components/HelloWorld/HelloWorld.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import './HelloWorld.Style.scss'
 
-const HelloWorld = () => <div> Hello World </div>
+class helloWorld extends React.Component {
+  render() {
+  return (
+    <div className="container">
+      <div className="row1">
+        <h1>Welcome To Qdex Pro</h1>
+      </div>
+      <div className="row2">
+        <button type="button">Create New Project</button>
+        <button type="button">Open Existing Project</button>
+      </div>
+      <div className="row3">
+        <div className="footer-left">
+          <h1>CopyRights</h1>
+          <h3>Sample Text for Copyrights</h3>
+        </div>
+        <div className="footer-right">
+          <h1>About</h1>
+          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</h3>
+        </div>
+      </div>
+    </div>
+  );
+  }
+}
 
-export default HelloWorld;
+export default helloWorld;
