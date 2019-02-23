d4edc61c527cece8ea8985f252e0a9b9f77226ed
Aravindhan Balamurugan <aravindhan.p.b@gmail.com>
2019-02-23 21:09:06 +0530

    Adding logic to prevent same dropdown option being selected

--- client/components/ClassMapper/classMapper.js
+++ client/components/ClassMapper/classMapper.js
@@ -17,22 +17,75 @@ const classOptions = [
 class ClassMapper extends React.Component{
   constructor(props){
     super(props);
-    this.state = {control: 1};
+    this.state = {control: 1,
+      classOptions:[
+      {
+        text: 'TextBoxMain',
+        value: 'TextBoxMain',
+        index:'null'},
+      {
+        text: 'ButtonMain',
+        value: 'buttonMain',
+        index:'null'},
+      {
+        text: 'TableMain',
+        value: 'TableMain',
+        index:'null'}
+      ]};
     this.handleClick = this.handleClick.bind(this);
     this.showControl = this.showControl.bind(this);
+    this.changeIndex = this.changeIndex.bind(this);
+    this.showDropDownText = this.showDropDownText.bind(this);
   }
 
   handleClick(event, data){
     this.setState({control:this.state.control+1});
   }
 
+  changeIndex(e,data,key){
+    let temp = this.state.classOptions;
+    //setting index for the current element and clearing it for the previous element
+    for (let i=0;i<this.state.classOptions.length;i++){
+      if(temp[i].index===key)
+      {
+        temp[i].index = 'null';
+      }
+    }
+    for (let i=0;i<this.state.classOptions.length;i++){
+      if(temp[i].value===data.value)
+      {
+        temp[i].index = key;
+      }
+    }
+    this.setState({classOptions:temp});
+  }
+
+  showDropDownText(key){
+    let temp = this.state.classOptions;
+    for (let i=0;i<this.state.classOptions.length;i++){
+      if(temp[i].index===key)
+      {
+        return temp[i].text;
+      }
+      else if (i===this.state.classOptions.length-1){
+        return 'Select'
+      }
+    }
+  }
+
   showControl(){
     let controlItems = [];
-    for (var i = 0; i < this.state.control; i++){
+    for (let i = 0; i < this.state.control; i++){
       controlItems.push(
         <div className='classMapper--content'>
           <Input placeholder='Enter Control Type' />
-          <div className='classMapper--dropdown'><Dropdown placeholder='Select Class' fluid selection options={classOptions} /></div>
+          <div className='classMapper--dropdown'>
+            <Dropdown
+              text = {this.showDropDownText(i)}
+              placeholder='Select Class'
+              selection
+              options={this.state.classOptions}
+              onChange={(e,data)=>this.changeIndex(e,data,i)}/></div>
         </div>
       );
     }
@@ -40,7 +93,6 @@ class ClassMapper extends React.Component{
   }
 
   render() {
-    console.log(this.state.control)
     return(
       <div className='classMapper'>
         {this.showControl()}
